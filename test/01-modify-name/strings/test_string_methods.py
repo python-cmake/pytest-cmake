49b20e4978f2cbe7e94f85b63a7ad826624f44a3
Ameya Vikram Singh <ameya.v.singh@gmail.com>
2025-01-09 20:10:59 +0530

    Add basic string addition method

--- test/01-modify-name/strings/test_string_methods.py
+++ test/01-modify-name/strings/test_string_methods.py
@@ -16,3 +16,11 @@ class TestStringMethods:
         assert s.split() == ["hello", "world"]
         with pytest.raises(TypeError):
             s.split(2)
+
+    @pytest.mark.parametrize("a, b, expected", [
+        ("5", "5", "55"),
+        ("10", "5", "105"),
+        ("0", "0", "00"),
+    ])
+    def test_strings_addition_with_params(self, a, b, expected):
+        assert a + b == expected
