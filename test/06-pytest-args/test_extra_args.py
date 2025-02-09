9d0734a70f26399a8d9dab019dba985d3bf9d827
Ameya Vikram Singh <ameya.v.singh@gmail.com>
2025-02-09 09:36:18 +0530

    Add an example of test with custom comandline flags
    Signed-off-by: Ameya Vikram Singh <ameya.v.singh@gmail.com>

--- test/06-pytest-args/test_extra_args.py
+++ test/06-pytest-args/test_extra_args.py
@@ -5,3 +5,11 @@ def test_requires_no_capture(request):
     if "--capture=no" not in request.config.invocation_params.args:
         pytest.fail("Test requires '--capture=no' to properly display output.")
     assert True
+
+
+def test_requires_args(cmdopt):
+    if cmdopt != None:
+        print(cmdopt)
+    else:
+        pytest.fail("Command line option not specified")
+    pass
